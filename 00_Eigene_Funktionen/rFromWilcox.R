59b2ee4ffaa46b552bdb855c9e6813e90c7355d8
Woosah <=>
2014-08-24 11:56:30 +0200

    hmmm

--- 00_Eigene_Funktionen/rFromWilcox.R
+++ 00_Eigene_Funktionen/rFromWilcox.R
@@ -1,5 +1,5 @@
 rFromWilcox <- function(wilcoxModel, N){
-	z <- qnorm(1 - wilcoxModel$p.value / 2)
+	z <- abs(qnorm(wilcoxModel$p.value / 2))
 	r <- z / sqrt(N)
 	# cat(wilcoxModel$data.name, "Effect Size, r = ", r, "\nZ-Value", z)
 	return(list(r = r, z = z))
